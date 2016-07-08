ea9c8934a1406736708a00e8b05ac64d724ee4d7
Florian Doublet <florian.doublet@hotmail.fr>
2016-07-08 13:38:34 +0200

    configurations for android studios

--- app/src/main/java/com/example/florian_doublet/pcsmsreader/utilities/IncommingSmsBroadcastReceiver.java
+++ app/src/main/java/com/example/florian_doublet/pcsmsreader/utilities/IncommingSmsBroadcastReceiver.java
@@ -3,7 +3,10 @@ package com.example.florian_doublet.pcsmsreader.utilities;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
 
 /**
  * Created by flori on 05/05/2016.
@@ -16,5 +19,32 @@ public class IncommingSmsBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         //something
+        if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
+            Bundle bundle = intent.getExtras();
+
+            SmsMessage[] msgs = null;
+
+            String str = "";
+
+            if (bundle != null) {
+                // Retrieve the SMS Messages received
+                Object[] pdus = (Object[]) bundle.get("pdus");
+                msgs = new SmsMessage[pdus.length];
+
+                // For every SMS message received
+                for (int i=0; i < msgs.length; i++) {
+                    // Convert Object array
+                    msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                    // Sender's phone number
+                    str += "SMS from " + msgs[i].getOriginatingAddress() + " : ";
+                    // Fetch the text message
+                    str += msgs[i].getMessageBody().toString();
+                    // Newline <img draggable="false" class="emoji" alt="🙂" src="https://s.w.org/images/core/emoji/72x72/1f642.png">
+                    str += "\n";
+                }
+
+            }
+        }
+
     }
 }
